5f6afd437fe547338962aad7e3239eff6a1c3d51
Lavinia Dusca <duscalavinia2@gmail.com>
2025-10-28 08:25:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6900668736-45260",
+  "appId": "1:966294973587:web:771815f5f2772a0618f166",
+  "apiKey": "AIzaSyBsDfq1ZyEQSBRYh2SixKZ0IJtH-a9-EtY",
+  "authDomain": "studio-6900668736-45260.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "966294973587"
+};
