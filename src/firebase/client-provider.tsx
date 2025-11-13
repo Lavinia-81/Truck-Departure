89e7e4b463f07657f116298a31b17fc0994c2516
Lavinia Dusca <duscalavinia2@gmail.com>
2025-11-13 11:19:08 +0000

    tot asa reactineaza. date care nu sunt permanente, dispar la refresh-ul

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,10 +1,26 @@
 'use client';
+import { useMemo } from 'react';
 import { FirebaseProvider, initializeFirebase } from '.';
+import { FirebaseApp } from 'firebase/app';
+import { Auth } from 'firebase/auth';
+import { Firestore } from 'firebase/firestore';
 
+// This is a re-implementation of the Firebase client provider that
+// is more robust and ensures that the Firebase app is initialized
+// only once.
 export function FirebaseClientProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <FirebaseProvider {...initializeFirebase()}>{children}</FirebaseProvider>;
+  const firebaseContext = useMemo((): {
+    firebaseApp: FirebaseApp;
+    auth: Auth;
+    firestore: Firestore;
+  } => {
+    return initializeFirebase();
+  }, []);
+  return (
+    <FirebaseProvider {...firebaseContext}>{children}</FirebaseProvider>
+  );
 }
