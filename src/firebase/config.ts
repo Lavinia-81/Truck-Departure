d43d60e1b20dce6fc50f217866adc67504bb6cec
Lavinia Dusca <duscalavinia2@gmail.com>
2025-11-13 20:37:51 +0000

    Try fixing this error: `Build Error: Export firebaseConfig doesn't exist

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,18 @@ import { FirebaseOptions } from 'firebase/app';
 // This is a placeholder configuration.
 // In a real-world scenario, this would be populated with your actual
 // Firebase project configuration from the Firebase console.
-export const firebaseConfig: FirebaseOptions = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID"
-};
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDhfcxEB0zImt9K8ddFKoz3zrQX9Swp01Y",
+  authDomain: "truck-app-project.firebaseapp.com",
+  databaseURL: "https://truck-app-project-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "truck-app-project",
+  storageBucket: "truck-app-project.firebasestorage.app",
+  messagingSenderId: "559055709371",
+  appId: "1:559055709371:web:c3e397b63454aca95c359e"
+};
